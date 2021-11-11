04ca6afbcf35125ec65e8f5cc29c3eefd98d145b
syahrolus <msyahrul15aulia@gmail.com>
2021-11-11 21:39:43 +0800

    update

---
+++ tes.sh
@@ -0,0 +1,67 @@
+mkdir controller/
+touch controller/config.php
+touch controller/peserta_create.php
+touch controller/peserta_delete.php
+touch controller/peserta_update.php
+touch controller/instruktur_create.php
+touch controller/instruktur_delete.php
+touch controller/instruktur_update.php
+touch controller/event_create.php
+touch controller/event_delete.php
+touch controller/event_update.php
+touch controller/bidang_create.php
+touch controller/bidang_delete.php
+touch controller/bidang_update.php
+touch controller/ke_create.php
+touch controller/ke_delete.php
+touch controller/ke_update.php
+touch controller/record_delete.php
+touch controller/record_update.php
+touch controller/up_delete.php
+touch controller/up_update.php
+
+mkdir libs/
+touch libs/style.css
+touch libs/script.js
+
+mkdir template/
+touch template/footer.php
+touch template/header.php
+
+mkdir view/
+touch view/dashboard.php
+touch view/login.php
+touch view/register.php
+
+mkdir view/events/
+touch view/events/index.php
+touch view/events/nambah_event.php
+touch view/events/edit_event.php
+
+mkdir view/peserta/
+touch view/peserta/index.php
+touch view/peserta/nambah_peserta.php
+touch view/peserta/edit_peserta.php
+
+mkdir view/instruktur/
+touch view/instruktur/index.php
+touch view/instruktur/nambah_instruktur.php
+touch view/instruktur/edit_instruktur.php
+
+mkdir view/bidang/
+touch view/bidang/index.php
+touch view/bidang/nambah_bidang.php
+touch view/bidang/edit_bidang.php
+
+mkdir view/kategori_event/
+touch view/kategori_event/index.php
+touch view/kategori_event/nambah_ke.php
+touch view/kategori_event/edit_ke.php
+
+mkdir view/record/
+touch view/record/index.php
+touch view/record/edit_record.php
+
+mkdir view/uang_peserta/
+touch view/uang_peserta/index.php
+touch view/uang_peserta/edit_up.php
